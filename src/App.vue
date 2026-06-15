8f21c12f9c97552318173143223cf56f96af5fca
Pavel Shalamitski <pshalamitski@supportheroes.io>
2026-06-15 19:16:46 +0300

    Add GitHub Pages util and version span

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,18 @@ const { isMobile } = useBreakpoint()
 </script>
 
 <template>
+  <!-- TODO: delete this span with CSS after final release -->
+  <span class="version">v 1.0</span>
   <MobileLanding v-if="isMobile" />
   <DesktopLanding v-else />
 </template>
+
+<style scoped>
+.version {
+  position: absolute;
+  color: #ffffff;
+  left: 0px;
+  top: 0px;
+  z-index: 99999;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //TODO: delete base after final release
   base: '/world-champ-landing/',
   plugins: [vue()],
   resolve: {
